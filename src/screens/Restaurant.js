cf5f3e3868c944ee19535dcbbbe4ca8f2f72835f
reepias <adthasit.coding@gmail.com>
2019-12-22 20:29:34 +0700

    Restaurant page

--- src/screens/Restaurant.js
+++ src/screens/Restaurant.js
@@ -1,23 +1,180 @@
 import React, {PureComponent} from 'react'
-import { StyleSheet } from 'react-native';
 import {
-  Container,
-  Header,
-  Content,
-  Text
-} from 'native-base'
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  ActivityIndicator,
+  Image,
+} from 'react-native'
+import {Container, Content} from 'native-base'
 
 import {HeaderComponent, FooterTabComponent} from '../components'
 
+import {ListItem} from 'react-native-elements'
+
+import _ from 'lodash'
+
+const API_KEY = 'AIzaSyAMSty4fKoaxuzS3bs0EQ-1oPIAozJY5hg'
+
 class Restaurant extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: false,
+      data: [],
+      pageToken: '',
+      refreshing: false,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = () => {
+    const latitude = '13.8235283'
+    const longitude = '100.5081204'
+    const {pageToken} = this.state
+    const urlFirst = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=AIzaSyAMSty4fKoaxuzS3bs0EQ-1oPIAozJY5hg
+    `
+    const urlNext = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=AIzaSyAMSty4fKoaxuzS3bs0EQ-1oPIAozJY5hg&pagetoken=${pageToken}`
+
+    let url = pageToken === '' ? urlFirst : urlNext
+    this.setState({loading: true})
+    fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        const arrayData = _.uniqBy([...this.state.data, ...res.results], 'id')
+
+        this.setState({
+          data: pageToken === '' ? res.results : arrayData,
+          loading: false,
+          refreshing: false,
+          pageToken: res.next_page_token,
+        })
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState({loading: false})
+      })
+  }
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%',
+        }}
+      />
+    )
+  }
+  renderHeader = () => {
+    return (
+      <Text
+        style={{
+          alignSelf: 'center',
+          fontWeight: 'bold',
+          fontSize: 20,
+          marginBottom: 10,
+        }}>
+        {this.state.siteTitle}
+      </Text>
+    )
+  }
+  renderFooter = () => {
+    if (this.state.pageToken === undefined) return null
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: '#CED0CE',
+        }}>
+        <ActivityIndicator animating size="large" />
+      </View>
+    )
+  }
+
+  handleRefresh = () => {
+    this.setState(
+      {
+        pageToken: '',
+        refreshing: true,
+      },
+      () => {
+        this.fetchData()
+      },
+    )
+  }
+
+  handleLoadMore = () => {
+    this.fetchData()
+  }
+
+  onBack = () => {
+    this.props.navigation.navigate('Main')
+  }
+
   render() {
     return (
       <Container style={styles.container}>
-        <HeaderComponent title={`Restaurants`} />
+        <HeaderComponent
+          onRefresh={this.handleRefresh}
+          onBack={this.onBack}
+          title={`Restaurants`}
+        />
         <Content>
-          <Text>Restaurants Page</Text>
+          <FlatList
+            data={this.state.data}
+            keyExtractor={item => item.id}
+            ListFooterComponent={this.renderFooter}
+            renderItem={({item}) => {
+              const rating = item.rating ? item.rating : 'na'
+              const photo = item.photos && item.photos[0].photo_reference
+              return (
+                <View style={{paddingVertical: 10, flex: 1}}>
+                  <View style={{alignItems: 'center'}}>
+                    {photo && (
+                      <Image
+                        style={styles.image}
+                        resizeMode="cover"
+                        source={{
+                          uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo}&key=${API_KEY}`,
+                        }}
+                      />
+                    )}
+                  </View>
+                  <ListItem
+                    title={`${item.name}` + ' (' + `${rating}` + ')'}
+                    subtitle={`${item.vicinity}`}
+                    containerStyle={{borderBottomWidth: 0}}
+                  />
+                  <View
+                    style={{
+                      height: 1,
+                      width: '100%',
+                      backgroundColor: '#CED0CE',
+                      marginBottom: 10,
+                    }}
+                  />
+                </View>
+              )
+            }}
+            onRefresh={this.handleRefresh}
+            refreshing={this.state.refreshing}
+            onEndReached={this.handleLoadMore}
+            onEndReachedThreshold={100}
+          />
         </Content>
-        <FooterTabComponent/>
+        <FooterTabComponent />
       </Container>
     )
   }
@@ -25,8 +182,13 @@ class Restaurant extends PureComponent {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 150 / 2,
+  },
 })
 
 export default Restaurant
--- src/screens/Test.js
+++ src/screens/Test.js
@@ -1,32 +1,27 @@
-import React, {PureComponent} from 'react'
-import {
-  Container,
-  Header,
-  Content,
-  Button
-} from 'native-base'
-import { Text } from 'react-native';
+import React from 'react'
+import {Container, Content, Button} from 'native-base'
+import {Text} from 'react-native'
 import {useNavigation} from 'react-navigation-hooks'
 
 import {HeaderComponent, FooterTabComponent} from '../components'
 
 export default () => {
-    const {navigate} = useNavigation()
+  const {navigate} = useNavigation()
 
-    const goRestaurant = () => {
-      navigate('RestaurantScreen')
-    }
+  const goRestaurant = () => {
+    navigate('RestaurantScreen')
+  }
 
-    return (
-      <Container style={{flex: 1}}>
-        <HeaderComponent title={`Test`} />
-        <Content>
-          <Text>Test Page</Text>
-          <Button onPress={goRestaurant}>
-            <Text>Go Restaurant Page</Text>
-          </Button>
-        </Content>
-        <FooterTabComponent/>
-      </Container>
+  return (
+    <Container style={{flex: 1}}>
+      <HeaderComponent title={`Test`} />
+      <Content>
+        <Text>Test Page</Text>
+        <Button onPress={goRestaurant}>
+          <Text>Go Restaurant Page</Text>
+        </Button>
+      </Content>
+      <FooterTabComponent />
+    </Container>
   )
 }
