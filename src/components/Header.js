fb8c8096268bc4ee61aab4fa8cd893ac2d15b19e
reepias <adthasit.coding@gmail.com>
2019-12-22 20:29:18 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Header, Left, Body, Right, Title} from 'native-base'
+import {Header, Left, Body, Right, Title, Button, Icon} from 'native-base'
 
 const HeaderComponent = props => {
   const {title} = props
@@ -8,11 +8,23 @@ const HeaderComponent = props => {
       style={{backgroundColor: 'green'}}
       androidStatusBarColor="black"
       iosBarStyle="dark-content">
-      <Left style={{flex: 1}}></Left>
+      <Left style={{flex: 1}}>
+        {props.onBack && (
+          <Button onPress={props.onBack} transparent>
+            <Icon name="arrow-back" />
+          </Button>
+        )}
+      </Left>
       <Body style={{flex: 1}}>
         <Title style={{alignSelf: 'center', color: '#FFF'}}>{title}</Title>
       </Body>
-      <Right style={{flex: 1}}></Right>
+      <Right style={{flex: 1}}>
+        {props.onRefresh && (
+          <Button onPress={props.onRefresh} transparent>
+            <Icon name="refresh" />
+          </Button>
+        )}
+      </Right>
     </Header>
   )
 }
