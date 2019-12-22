0db776a1776636a2dfd76231fa3d0b332d9c8a81
reepias <adthasit.coding@gmail.com>
2019-12-22 20:29:11 +0700

    Update FooterTab.js

--- src/components/FooterTab.js
+++ src/components/FooterTab.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import {Footer, FooterTab, Button, View} from 'native-base'
-import { Text } from 'react-native';
+import {Text} from 'react-native'
 
 const FooterComponent = props => {
   return (
     <Footer>
-        <FooterTab>
-        <View disabled style={{flex: 1, justifyContent: 'center', paddingLeft: 15, backgroundColor: 'green'}} active>
-            <Text style={{ color: 'white', fontFamily: 'Sarabun-Regular', fontSize: 18}}>Adthasit Suksomthawisap</Text>
+      <FooterTab>
+        <View
+          disabled
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            paddingLeft: 15,
+            backgroundColor: 'green',
+          }}
+          active>
+          <Text
+            style={{
+              color: 'white',
+              fontFamily: 'Sarabun-Regular',
+              fontSize: 18,
+            }}>
+            Adthasit Suksomthawisap
+          </Text>
         </View>
-        </FooterTab>
+      </FooterTab>
     </Footer>
   )
 }
